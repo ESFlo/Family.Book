1d9ce7c32351951b0b8c6ea5ca6be11945e5b415
elifbusracebeci <elifbusracebeci@elifs-air.fritz.box>
2020-07-13 19:35:02 +0200

    Kommentare Klasse LoginController

--- src/main/java/Family/Book/LoginController.java
+++ src/main/java/Family/Book/LoginController.java
@@ -11,18 +11,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+/**
+ * Die LoginController.java Klasse, ist die erste Klasse die beim Start des
+ * Programmes durchgeführt wird
+ *
+ *
+ */
 
 @RestController
 @CrossOrigin
-public class LoginController { // MUSS WIE DIE JETZIGE KLASSE HEIßEN
+public class LoginController {
+
+	/**
+	 * Initialisierung des LOGGER`s
+	 */
 
 	private static final Logger LOGGER = LogManager.getLogger(LoginController.class);
 
-	@GetMapping("test")
-	public String test() {
-		return "test";
-	}
+	/**
+	 * 
+	 * Bei korrekter Durchführung ohne Fehlermeldung, landet man auf der
+	 * indexlog.html und bekommt in der Console die Logger-Info: "User on
+	 * indexlog.html" falls nicht, dann erscheint in der Console die Logger.error
+	 * Meldung "IOException in ViewController forwardToIndex method"
+	 * 
+	 */
 
 	@RequestMapping("/")
 	public void forwardToIndex(HttpServletResponse http) {
@@ -34,6 +47,15 @@ public class LoginController { // MUSS WIE DIE JETZIGE KLASSE HEIßEN
 		}
 	}
 
+	/**
+	 * 
+	 * Durch Klick auf den Button Login, meldet man sich ein - man gelangt auf die
+	 * kontaktErstellen.html und in der Console erscheint die LOGGER.info "Login
+	 * erfolgreich" falls nicht - erscheint in der Console die LOGGER.error Meldung
+	 * "IOException in Controller login method"
+	 * 
+	 */
+
 	@GetMapping("login")
 	public void login(HttpServletResponse http) {
 		try {
@@ -43,6 +65,14 @@ public class LoginController { // MUSS WIE DIE JETZIGE KLASSE HEIßEN
 			LOGGER.error("IOException in Controller login method: " + e);
 		}
 	}
+
+	/**
+	 * 
+	 * Durch Klick auf den Reiter Lagout in der Navigationleiste meldet man sich an
+	 * - man gelangt auf die indexlog.html und in der Console erscheint die
+	 * LOGGER.info "Logout war erfolgreich" falls nicht - erscheint in der Console
+	 * die LOGGER.error Meldung "IOException in Controller logout method"
+	 */
 
 	@GetMapping("logout")
 	public void logout(HttpServletResponse http) {
