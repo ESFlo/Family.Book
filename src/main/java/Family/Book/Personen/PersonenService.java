3a040a79035a28abfecaa9807bd9afe97ce18bc8
elifbusracebeci <elifbusracebeci@elifs-air.fritz.box>
2020-07-13 20:25:38 +0200

    Kommentare PersonenService hinzugefügt

--- src/main/java/Family/Book/Personen/PersonenService.java
+++ src/main/java/Family/Book/Personen/PersonenService.java
@@ -1,6 +1,5 @@
 package Family.Book.Personen;
 
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Iterator;
@@ -10,49 +9,67 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
-
-
 @Service
 public class PersonenService {
-	
+	/**
+	 * Initialisierung des Loggers und Variablen Deklaration um Logger schreiben zu
+	 * koennen
+	 */
 	private static final Logger LOGGER = LogManager.getLogger(PersonenService.class);
-	
+
 	@Autowired
 	PersonenRepository personenRepository;
-	
-	//Beziehung
+
+	// Beziehung
+
+	/**
+	 * Alle gespeicherten Personen ausgeben lassen Zunaechst wird eine leere
+	 * List @param Personenlist erstellt, dannach werden alle Personen aus dem
+	 * Repository gesucht Personen werden nacheinander in die @param personenlist
+	 * gespeichert Personenlist wird nachdem alle Personen gespeichert sind returned
+	 */
 	public List<Personen> getAllPersonen() {
 		Iterator<Personen> it = personenRepository.findAll().iterator();
 		ArrayList<Personen> result = new ArrayList<>();
-		while(it.hasNext()) {
+		while (it.hasNext()) {
 			result.add(it.next());
 		}
 		return result;
 	}
-	
-	
+
 	public Personen getPersonById(int id) {
 		return personenRepository.findById(id).orElse(null);
 	}
-	
+
+	/**
+	 * @return Basismethoden zum erstellen, aendern, loeschen von Personen Zum
+	 *         Dokumentieren - Logger
+	 */
+
 	public void createPerson(Personen personen) {
 		personenRepository.save(personen);
-		LOGGER.warn("Person erstellt");
+		LOGGER.info("Person erstellt");
 	}
-	
+
 	public void updatePerson(int id, Personen personen) {
 		personenRepository.save(personen);
-		LOGGER.info(
-				"Person geändert: " + personen.getId() + " " + personen.getNachname() + " " + personen.getVorname());
+		LOGGER.info("Person geaendert");
 	}
-	
+
 	public void deletePerson(int id) {
 		personenRepository.deleteById(id);
 		LOGGER.warn("Person gelöscht");
 	}
-	
-	//Verwandt
+
+	// Verwandt
+
+	/**
+	 * Alle gespeicherten PersonenOfVerwandPersonen ausgeben lassen Zunaechst wird
+	 * eine leere List @param Personenlist erstellt, dannach werden alle Personen
+	 * aus dem Repository gesucht ersonenOfVerwandPersonen werden nacheinander in
+	 * die @param personenlist gespeichert Personenlist wird nachdem alle Personen
+	 * gespeichert sind returned
+	 */
 	public List<Personen> getAllPersonenOfVerwandPersonen(String verwandPersonen) {
 		return personenRepository.findByVerwandPersonen(verwandPersonen);
 	}
@@ -60,86 +77,120 @@ public class PersonenService {
 	public List<Personen> getAllPersonenOfVerwandPersonen() {
 		Iterator<Personen> it = personenRepository.findAll().iterator();
 		ArrayList<Personen> result = new ArrayList<>();
-		while(it.hasNext()) {
+		while (it.hasNext()) {
 			result.add(it.next());
 		}
-		return result;	}
+		return result;
+	}
 
+	/**
+	 * @return Basismethoden zum erstellen und aendern von Verwandte Zum
+	 *         Dokumentieren - Logger
+	 */
 	public void createVerwand(Personen verwandPersonen) {
 		personenRepository.save(verwandPersonen);
-		
+		LOGGER.info("Verwandt erstellt");
+
 	}
-	
+
 	public void updateVerwand(int id, Personen verwandPersonen) {
 		personenRepository.save(verwandPersonen);
-		
+		LOGGER.info("Verwandt geaendert");
+
 	}
-	//Freunde
-	public List<Personen> getAllPersonenOfFreundePersonenById(String freundePersonen){
+
+	// Freunde
+
+	/**
+	 * Alle gespeicherten PersonenOfFreundePersonenById ausgeben lassen Zunaechst
+	 * wird eine leere List @param Personenlist erstellt, dannach werden alle
+	 * Personen aus dem Repository gesucht PersonenOfFreundePersonenById werden
+	 * nacheinander in die @param personenlist gespeichert Personenlist wird nachdem
+	 * alle Personen gespeichert sind returned
+	 */
+
+	public List<Personen> getAllPersonenOfFreundePersonenById(String freundePersonen) {
 		return personenRepository.findByFreundePersonen(freundePersonen);
 	}
-	
+
 	public List<Personen> getAllPersonenOfFreundePersonen() {
 		Iterator<Personen> it = personenRepository.findAll().iterator();
 		ArrayList<Personen> result = new ArrayList<>();
-		while(it.hasNext()) {
+		while (it.hasNext()) {
 			result.add(it.next());
 		}
-		return result;	}
+		return result;
+	}
+
 	public List<Personen> getFreunde() {
 		Iterator<Personen> it = personenRepository.findAll().iterator();
 		ArrayList<Personen> result = new ArrayList<>();
-		while(it.hasNext()) {
+		while (it.hasNext()) {
 			result.add(it.next());
 		}
-		return result;	}
+		return result;
+	}
+
+	/**
+	 * @return Basismethoden zum erstellen, aendern, loeschen von Freunde Zum
+	 *         Dokumentieren - Logger
+	 */
+
 	public void createFreunde(Personen freundePersonen) {
 		personenRepository.save(freundePersonen);
-		
+		LOGGER.info("Freund erstellt");
+
 	}
-	
+
 	public void updateFreunde(int id, Personen freundePersonen) {
 		personenRepository.save(freundePersonen);
-		
+		LOGGER.info("Freund geaendert");
+
 	}
-	
-	public void deleteFreunde(int id,Personen freundePersonen) {
+
+	public void deleteFreunde(int id, Personen freundePersonen) {
 		personenRepository.delete(freundePersonen);
-		
+		LOGGER.warn("Freund geloescht");
+
 	}
-	
-	//Bekannte
-	public List<Personen> getAllPersonenOfBekanntePersonen(String bekanntePersonen){
+
+	// Bekannte
+
+	/**
+	 * Alle gespeicherten PersonenOfBekanntePersonen ausgeben lassen Zunaechst wird
+	 * eine leere List @param Personenlist erstellt, dannach werden alle Personen
+	 * aus dem Repository gesucht PersonenOfBekanntePersonen werden nacheinander in
+	 * die @param personenlist gespeichert Personenlist wird nachdem alle Personen
+	 * gespeichert sind returned
+	 */
+
+	public List<Personen> getAllPersonenOfBekanntePersonen(String bekanntePersonen) {
 		return personenRepository.findByBekanntePersonen(bekanntePersonen);
 	}
 
 	public List<Personen> getAllPersonenOfBekanntePersonen() {
 		Iterator<Personen> it = personenRepository.findAll().iterator();
 		ArrayList<Personen> result = new ArrayList<>();
-		while(it.hasNext()) {
+		while (it.hasNext()) {
 			result.add(it.next());
 		}
-		return result;	}
+		return result;
+	}
 
+	/**
+	 * @return Basismethoden zum erstellen und aendern von Bekannte Zum
+	 *         Dokumentieren - Logger
+	 */
 	public void createBekannte(Personen bekanntePersonen) {
 		personenRepository.save(bekanntePersonen);
-		
+		LOGGER.info("Bekannte erstellt");
+
 	}
-	
+
 	public void updateBekannte(int id, Personen bekanntePersonen) {
 		personenRepository.save(bekanntePersonen);
-		
+		LOGGER.info("Bekannte geaendert");
+
 	}
-
-	
-
-
-	
-
-
-	
-
-
-	
 
 }
